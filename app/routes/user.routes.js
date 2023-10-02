306a4178f6cb550d46fa5a0346b8002817048c5d
Naphat <naphat_g@MacBook-Air-khxng-Naphat.local>
2023-10-03 01:40:28 +0700

    first commit

---
+++ app/routes/user.routes.js
@@ -0,0 +1,10 @@
+const authJwt = require("../middleware/auth.jwt");
+module.exports = (app)=>{
+    const user_controller = require("../controllers/user.controller");
+    var router = require("express").Router();
+    router.get("/:us", user_controller.validUsername);
+    router.post("/signup", user_controller.createNewUser);
+    router.post("/login", user_controller.login);
+    router.get("/", authJwt, user_controller.getAllUsers);
+    app.use("/api/auth", router);
+};
